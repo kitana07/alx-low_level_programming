55d76520c5f9cf612d633a5c77bb5ba568ed27d4
Michaela <m.armino91@gmail.com>
2023-04-21 16:25:55 -0700

    Positive, negative and zero - removed space, 2nd attempt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 /**
  * main - determine if number is positive, negative or zero
- * 
- * Return: Always 0 (Success)
+ * Return: Always (Success)
  */
 
 int main(void)
@@ -15,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
