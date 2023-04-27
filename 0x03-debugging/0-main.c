723e5f8177dc91f80df56aef9ee2cac8501475da
Michaela <m.armino91@gmail.com>
2023-04-27 10:52:44 -0700

    Corrected spacing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - Tests the function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
+
 {
+
 	int i;
 
 	i = 0;
