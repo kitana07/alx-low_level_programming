00420ea4126c3718eeda147df56e7ccb8d52840e
Michaela <m.armino91@gmail.com>
2023-04-28 14:20:16 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
