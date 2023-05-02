38467c4b6d5fa033f5d9038c49438978de56e7ec
Michaela <m.armino91@gmail.com>
2023-05-02 16:58:16 -0700

    Removed whitespaces

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int m;
-	
+
 	for (m = 0; src[m] != '\0'; m++)
 	{
 		dest[m] = src[m];
 	}
-		dest[m++] = '\0';
+	dest[m++] = '\0';
 	return (dest);
 }
