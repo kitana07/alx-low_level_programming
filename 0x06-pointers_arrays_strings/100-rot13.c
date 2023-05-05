d5017324d772262fdbe7896696a0753d22ce0b53
Michaela <m.armino91@gmail.com>
2023-05-04 18:29:37 -0700

    Encode string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * rot13 - Encode a string
+  * @c: string
+  * Return: c
+  */
+
+char *rot13(char *c)
+{
+	int m = 0;
+
+	while (c[m])
+	{
+		while ((c[m] >= 'a' && c[m] <= 'z') || (c[m] >= 'A' && c[m] <= 'Z'))
+		{
+			if ((c[m] > 'a' && c[m] <= 'z') || (c[m] > 'A' && c[m] <= 'Z'))
+			{
+				c[m] -= 13;
+				break;
+			}
+			c[m] += 13;
+			break;
+		}
+		m++;
+	}
+	return (c);
+}
