390539650fe7181897aab176cf5bdf74e0cdc1d7
Michaela <m.armino91@gmail.com>
2023-05-08 20:09:22 -0700

    checked descr

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - set point value to a char
+ * set_string - Entry point
  * @s: char 
  * @to: value to change
+ * Return: Always 0 (Success)
  */
 
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
