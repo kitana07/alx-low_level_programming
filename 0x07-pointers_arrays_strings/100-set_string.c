74ec72ddbcc5e5c322708a9d8605f15f2d78eab2
Michaela <m.armino91@gmail.com>
2023-05-08 19:42:36 -0700

    Change value to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set point value to a char
+ * @s: value
+ * @to: value to change
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
