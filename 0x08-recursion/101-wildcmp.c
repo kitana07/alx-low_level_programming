97a6780e04a0237eb07037b738fcbc90a8d32748
Michaela <m.armino91@gmail.com>
2023-05-10 18:42:32 -0700

    Corrected code

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,6 +9,15 @@
 
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0')
+        {
+                if (*s2 != '\0' && *s2 == '*')
+                {
+                return (wildcmp(s1, s2 + 1));
+                }
+        return (*s2 == '\0');
+        }
+
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
@@ -17,14 +26,5 @@ int wildcmp(char *s1, char *s2)
 	{
 	return (wildcmp(s1 + 1, s2 + 1));
 	}
-
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-		return (wildcmp(s1, s2 + 1));
-		}
-	return (*s2 == '\0');
-	}
-		return (0);
+	return (0);
 }
