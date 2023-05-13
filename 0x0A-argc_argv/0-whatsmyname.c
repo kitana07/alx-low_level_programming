4a304e96a69ae92f4dee1585f8298d45969b46e3
Michaela <m.armino91@gmail.com>
2023-05-12 18:30:58 -0700

    Correct space

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the name
@@ -11,6 +11,5 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
 return (0);
 }
