b4d9c819bd966cd0a33b37518d351a3786ac1693
Michaela <m.armino91@gmail.com>
2023-05-12 18:17:47 -0700

    Print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name
+ * @argc: argument amount
+ * @argv: argument array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+	
+	printf("%s\n", argv[0]);
+
+return (0);
+}
