aa1cc668d2ceb188190f36efca28220af4377bdb
Michaela <m.armino91@gmail.com>
2023-05-23 03:21:38 -0700

    added space

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,8 @@ struct dog
 /**
  * dog_t - typedef
  */
-
 typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
